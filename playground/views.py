8ee632b2961fcf89eb3ccbe7497f0ae67ade9443
yhunlu <yhunlu87@gmail.com>
2022-08-19 04:51:10 +0300

    example snacks for using template.

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def say_hello(request):
-    return HttpResponse("Hello World")
+    return render(request, 'hello.html', { 'name': 'yahya'})
