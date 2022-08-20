1203dc8937b2a841413c2b2aa607d0006a98e41e
yhunlu <yhunlu87@gmail.com>
2022-08-20 03:46:16 +0300

    defined customer model

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -7,3 +8,10 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
 
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
