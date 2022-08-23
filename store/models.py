cad60a7eb6e4dc316f267f2017096c17e043b747
yhunlu <yhunlu87@gmail.com>
2022-08-23 05:51:00 +0300

    generated migrations.

--- store/models.py
+++ store/models.py
@@ -14,6 +14,7 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
