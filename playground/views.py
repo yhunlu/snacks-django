1c6bca28d6f764991bd2f204d374e5f1b13ea77c
yhunlu <yhunlu87@gmail.com>
2022-08-28 04:45:07 +0300

    (snacks) used sorted in ORM.

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 
 def say_hello(request):
-    # Products: inventory < 10 OR price < 20
-    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    queryset = Product.objects.order_by('unit_price', '-title').reverse()
 
     return render(request, 'hello.html', {'name': 'yahya', 'results': list(queryset)})
