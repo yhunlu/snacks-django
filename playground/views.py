9e5644cd78aece8fcdfc0d5e6d35a5805a497820
yhunlu <yhunlu87@gmail.com>
2022-08-29 06:11:31 +0300

    (snacks) used ORM to get ordered products.sub-query.

--- playground/views.py
+++ playground/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    # related fields and inner join
-    # queryset = Product.objects.values('id', 'title', 'collection__title')
-    queryset = Product.objects.values_list('id', 'title', 'collection__title')
+    # select product which are already ordered.
+    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
 
-    return render(request, 'hello.html', {'name': 'yahya', 'results': list(queryset)})
+    return render(request, 'hello.html', {'name': 'yahya', 'results': list(queryset)}) 
