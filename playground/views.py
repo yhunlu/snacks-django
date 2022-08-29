972242b1d15065d36e9211987e4f7b7146d6ebb9
yhunlu <yhunlu87@gmail.com>
2022-08-30 02:32:26 +0300

    (snacks) using select_related, prefetch_related and together.

--- playground/views.py
+++ playground/views.py
@@ -6,7 +6,8 @@ from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    # queryset = Product.objects.only('id', 'title')
-    queryset = Product.objects.defer('description')
+    # queryset = Product.objects.select_related('collection').all()
+    # queryset = Product.objects.prefetch_related('promotions').all()
+    queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()
 
     return render(request, 'hello.html', {'name': 'yahya', 'results': list(queryset)})
