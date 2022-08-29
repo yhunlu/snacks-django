b6f47e7dc2c184ed05f8715e71f831d3f0c0194e
yhunlu <yhunlu87@gmail.com>
2022-08-30 02:55:36 +0300

    (snacks) complex ORM query related 4 tables.

--- playground/views.py
+++ playground/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
-from store.models import Product, OrderItem
+from store.models import Product, Order
 
 
 def say_hello(request):
-    # queryset = Product.objects.select_related('collection').all()
-    # queryset = Product.objects.prefetch_related('promotions').all()
-    queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()
+    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
 
     return render(request, 'hello.html', {'name': 'yahya', 'results': list(queryset)})
