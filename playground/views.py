598bb3bd3d0cae9fd695c6a2dded1d85c1397399
yhunlu <yhunlu87@gmail.com>
2022-09-04 23:00:28 +0300

    (snacks) grouping data.

--- playground/views.py
+++ playground/views.py
@@ -3,20 +3,14 @@ from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
-from django.db.models import Value, F, Func
+from django.db.models import Value, F, Func, Count
 from django.db.models.functions import Concat
 from store.models import Customer, Product, Order
  
 
 def say_hello(request):
     queryset = Customer.objects.annotate(
-        # CONCAT
-        full_name=Func(F('first_name'), Value(' ') ,F('last_name'), function='CONCAT')
-    )
-
-    queryset = Customer.objects.annotate(
-        # CONCAT
-        full_name=Concat('first_name', Value(' ') ,'last_name')
+        orders_count=Count('order')
     )
 
     return render(request, 'hello.html', {'name': 'yahya', 'results': list(queryset)})
