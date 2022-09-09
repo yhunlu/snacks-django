c5eab4803bf66c79034abd5ab97b67a03c2855ff
yhunlu <yhunlu87@gmail.com>
2022-09-10 00:49:33 +0300

    (snacks) querying generic relationships, using contenttypes.

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F
-from django.db.models.aggregates import Count, Max, Min, Avg, Sum
-from django.db.models import Value, F, Func, Count, ExpressionWrapper, DecimalField
-from django.db.models.functions import Concat
-from store.models import Customer, Product, Order
- 
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
-    queryset = Product.objects.annotate(
-        discounted_price= discounted_price
+    content_type = ContentType.objects.get_for_model(Product)
+
+    queryset = TaggedItem.objects \
+    .select_related('tag') \
+    .filter(
+        content_type=content_type,
+        object_id=1
     )
 
     return render(request, 'hello.html', {'name': 'yahya', 'results': list(queryset)})
