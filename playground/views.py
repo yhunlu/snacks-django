3c329971c3e2bd7470168d754b210f77e4977ee4
yhunlu <yhunlu87@gmail.com>
2022-09-15 23:58:44 +0300

    (snacks) transactions.

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.db import transaction
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product, Collection
+from store.models import Product, Collection, Order, OrderItem
 from tags.models import TaggedItem
 
 
 def say_hello(request):
-    Collection.objects.filter(id__gt=5).delete()
+
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 1
+        order.save()
+
+        item = OrderItem()
+        item.order = order
+        item.product_id = 1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
 
     return render(request, 'hello.html', {'name': 'yahya', 'results': 'list(queryset)'})
