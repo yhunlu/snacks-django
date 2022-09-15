22b6855d4af9b59e55be6d512b7d3c879beb4303
yhunlu <yhunlu87@gmail.com>
2022-09-15 23:42:16 +0300

    (snacks) updating objects.

--- playground/views.py
+++ playground/views.py
@@ -5,9 +5,7 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    collection = Collection()
-    collection.title = 'Video Games'
-    collection.featured_product = Product(pk=1)
-    collection.save()
 
-    return render(request, 'hello.html', {'name': 'yahya', 'results': list(queryset)})
+    Collection.objects.filter(pk=11).update(title="Mike")
+
+    return render(request, 'hello.html', {'name': 'yahya', 'results': 'list(queryset)'})
