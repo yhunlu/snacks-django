4520eae7f0d70d2081e12d12351c213ae20b62f7
yhunlu <yhunlu87@gmail.com>
2022-09-16 00:07:25 +0300

    (snacks) lunching RAW SQL QUERIES.

--- playground/views.py
+++ playground/views.py
@@ -3,20 +3,11 @@ from django.db import transaction
 from django.contrib.contenttypes.models import ContentType
 from store.models import Product, Collection, Order, OrderItem
 from tags.models import TaggedItem
-
+from django.db import connection
 
 def say_hello(request):
+    with connection.cursor() as cursor:
+        cursor.execute()
+        cursor.callproc('get_customer', [1, 3, 4])
 
-    with transaction.atomic():
-        order = Order()
-        order.customer_id = 1
-        order.save()
-
-        item = OrderItem()
-        item.order = order
-        item.product_id = 1
-        item.quantity = 1
-        item.unit_price = 10
-        item.save()
-
-    return render(request, 'hello.html', {'name': 'yahya', 'results': 'list(queryset)'})
+    return render(request, 'hello.html', {'name': 'yahya', 'results': list(queryset)})
