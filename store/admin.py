2069eb45f2eab0e07a1bc5f61c4a4b3269ea6b05
yhunlu <yhunlu87@gmail.com>
2022-10-02 02:31:43 +0300

    create new control table as customer from customer models.

--- store/admin.py
+++ store/admin.py
@@ -4,5 +4,14 @@ from . import models
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 10
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
 
 admin.site.register(models.Collection)
