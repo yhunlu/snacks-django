f2dde56234e4b1358634a00d14a5f3eee6b3cb20
yhunlu <yhunlu87@gmail.com>
2022-10-03 02:47:11 +0300

    providing links to other pages.

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 from . import models
+
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
@@ -8,16 +11,25 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+        url = (
+            reverse('admin:store_product_changelist')
+            + '?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            })
+        )
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
-            products_count = Count('product')
+            products_count=Count('product')
         )
+
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
     list_select_related = ['collection']
@@ -31,12 +43,14 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'OK'
 
+
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
