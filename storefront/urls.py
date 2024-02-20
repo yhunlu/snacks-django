c86e49ece540bddb5cc7c0c21f4d2425b188208d
YAHYA UNLU <yhunlu87@gmail.com>
2024-02-20 23:08:59 +0300

    Creating API Views

--- storefront/urls.py
+++ storefront/urls.py
@@ -13,15 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
-admin.site.site_header = 'Storefront Admin'
-admin.site.index_title = 'Admin'
+admin.site.site_header = "Storefront Admin"
+admin.site.index_title = "Admin"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path("admin/", admin.site.urls),
+    path("playground/", include("playground.urls")),
+    path("store/", include("store.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
