a3faf843556cfb3af5e86a0bd517f22198f6b260
YAHYA UNLU <yhunlu87@gmail.com>
2024-02-21 16:56:35 +0300

    Serializing Objects

--- store/serializers.py
+++ store/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
