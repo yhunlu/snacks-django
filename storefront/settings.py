3b72a4b3aac76b087f52bbb776ff43ecfff43082
Yahya Unlu <yhunlu87@gmail.com>
2024-03-31 01:41:26 +0300

    Extending the Abstract User Class

--- storefront/settings.py
+++ storefront/settings.py
@@ -46,9 +46,9 @@ INSTALLED_APPS = [
     "playground",
     "debug_toolbar",
     "store",
-    "store_custom",
     "tags",
     "likes",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -147,3 +147,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
 }
+
+AUTH_USER_MODEL = 'core.User'
