1e53197f42e0971da50b65bae075daff30d29ad8
YAHYA UNLU <yhunlu87@gmail.com>
2024-07-30 01:00:25 +0300

    applying permissions to order

--- store/views.py
+++ store/views.py
@@ -151,5 +151,14 @@ class CustomerViewSet(ModelViewSet):
 
 
 class OrderViewSet(ModelViewSet):
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_staff:
+            return Order.objects.all()
+
+        (customer_id, created) = Customer.objects.only("id").get_or_create(user_id=user.id)
+        return Order.objects.filter(customer_id=customer_id)
